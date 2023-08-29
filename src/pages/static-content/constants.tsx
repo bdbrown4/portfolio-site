7eb501103752f22e08460947b8f9492b70092f08
Ben Brown <bdbrown4@eagles.usi.edu>
2023-08-29 10:21:06 -0400

    lint fix

--- src/pages/static-content/constants.tsx
+++ src/pages/static-content/constants.tsx
@@ -1,5 +1,5 @@
 import { BsGithub, BsLinkedin, BsDiscord, BsFacebook, BsInstagram  } from "react-icons/bs";
-type IconElement = { [key: string] : JSX.Element };
+type IconElement = Record<string, JSX.Element>;
 export const iconElements: IconElement = {
     "4f3e41ec-4666-11ee-abfb-be74eed77460": <BsGithub className="dark:text-white xl:text-3xl lg:text-3xl text-2xl cursor-pointer" />,
     "c139e970-4666-11ee-abfb-be74eed77460": <BsLinkedin className="dark:text-white xl:text-3xl lg:text-3xl text-2xl cursor-pointer" />,
