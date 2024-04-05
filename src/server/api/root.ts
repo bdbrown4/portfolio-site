6070a762a9ef302deca56e320fdc8bf9438a14ab
Ben Brown <bdbrown4@eagles.usi.edu>
2024-04-05 11:11:55 -0400

    removing prisma for now

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { iconRouter } from "./routers/iconRouter";
+import { api } from "~/utils/api";
 
 /**
  * This is the primary router for your server.
